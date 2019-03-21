565d56feaf548b2e33a460ae1da5f83b0e08a475
rskinner <reggie.skinnerjr@gmail.com>
2019-03-21 18:04:05 -0400

    Noah!!!!

--- ThisOneWorks.py
+++ ThisOneWorks.py
@@ -4,7 +4,6 @@ def main(hostname,username,password):
         remote = SSHClient()
         remote.load_system_host_keys()
         remote.connect(hostname=hostname, port=22, username=username, password=password)
-        remote.connect.close()
 hostname = raw_input('Hostname:')
 username = raw_input('Username:')
 password = getpass.getpass("Password: ")
