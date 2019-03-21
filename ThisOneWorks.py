97fab2c77427647df35cf5ef52ef2f7a12063118
rskinner <reggie.skinnerjr@gmail.com>
2019-03-21 17:59:32 -0400

    Noah!!!!

--- ThisOneWorks.py
+++ ThisOneWorks.py
@@ -1,18 +1,12 @@
-from pan import xapi
 from paramiko import SSHClient
 import getpass
-
-def connect():
-    hostname = input("Hostname:")
-    username = input("Username:")
-    password = getpass.getpass("Password: ")
-    remote = SSHClient()
-    remote.load_system_host_keys()
-    remote.connect(hostname=hostname, port=22, username=username, password=password)
-    remote.connect.close()
-
-def main():
-    connect()
-
+def main(hostname,username,password):
+        remote = SSHClient()
+        remote.load_system_host_keys()
+        remote.connect(hostname=hostname, port=22, username=username, password=password)
+        remote.connect.close()
+hostname = raw_input('Hostname:')
+username = raw_input('Username:')
+password = getpass.getpass("Password: ")
 if __name__ == '___main___':
     main()
