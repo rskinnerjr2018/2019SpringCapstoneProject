06d978bf5ee648cac80a89ba5198be9ace0ab9fe
rskinner <reggie.skinnerjr@gmail.com>
2019-03-21 18:37:56 -0400

    Noah!!!!

--- Test.py
+++ Test.py
@@ -2,24 +2,20 @@
 from pan import xapi
 from paramiko import SSHClient
 import getpass
-Username = input("Username:")
+hostname = raw_input('Hostname:')
+Username = raw_input("Username:")
 password = getpass.getpass("Password: ")
-port = 22
-
-remote = SSHClient()
-remote.load_system_host_keys()
-
-remote.connect(hostname='172.16.203.251', port=22, username=Username, password=password)
-
-
+def connection(hostname,username,password)
+    remote = SSHClient()
+    remote.load_system_host_keys()
+    remote.connect(hostname=hostname, port=22, username=username, password=password)
 def main():
     def __init__(self, arg1=None):
         self.arg1 = arg1
         xapi.PanXapi.cmd_xml(self, cmd='<show><system><info></info></system></show>')
-        return
-
-
+        print(xapi.PanXapi.cmd_xml(self,cmd='<show><system><info></info></system></show>'))
 
 # Call main
 if __name__ == '__main__':
+    connection()
     main()
