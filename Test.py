4aeccd6143b0af6d2a07e989e85bdf13e11f8205
rskinner <reggie.skinnerjr@gmail.com>
2019-03-23 11:54:23 -0400

    uhhhhhh

--- Test.py
+++ Test.py
@@ -7,22 +7,17 @@ import os
 hostname = raw_input('Hostname:')
 username = raw_input("Username:")
 password = getpass.getpass("Password: ")
-x = raw_input('File Path and Name:')
+filename = raw_input(('File Path/Name:'))
 def connection(hostname,username,password):
     remote = SSHClient()
     remote.load_system_host_keys()
     remote.connect(hostname=hostname, port=22, username=username, password=password)
-def main():
-    def __init__(self,x,filepath, arg1=None):
-        self.arg1 = arg1
-        xapi.PanXapi.cmd_xml(self, cmd='<show><system><info></info></system></show>')
-        filepath = os.path.join('x')
-        if not os.path.exists('x')
-            os.makedirs('x')
-        f = open(filepath, 'w')
-        print(xapi.PanXapi.cmd_xml(self,cmd='<show><system><info></info></system></show>'))
+def file(filename):
+    create_file= open(filename,'w')
+    create_file.write('hello')
+
 
 # Call main
 if __name__ == '__main__':
     connection(hostname,username,password)
-    main()
+    file(filename)
