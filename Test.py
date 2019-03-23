e97a22302980701a2d346209d552b6b12da6d4c1
rskinner <reggie.skinnerjr@gmail.com>
2019-03-23 12:21:18 -0400

    We are working Noah

--- Test.py
+++ Test.py
@@ -2,14 +2,20 @@
 from pan import xapi
 from paramiko import SSHClient
 import getpass
-hostname = raw_input('Hostname:')
-username = raw_input("Username:")
-password = getpass.getpass("Password: ")
-filename = raw_input(('File Path/Name:'))
+#hostname = raw_input('Hostname:')
+#username = raw_input("Username:")
+#password = getpass.getpass("Password: ")
+#filename = raw_input(('File Path/Name:'))
+generate_key = raw_input('Do you want to generate an API Key:?')
 def connection(hostname,username,password):
     remote = SSHClient()
     remote.load_system_host_keys()
     remote.connect(hostname=hostname, port=22, username=username, password=password)
+def api_call(gnerate_key, self):
+    self.xapi.Panxapi.keygen = None
+    if (generate_key == 'y') or (generate_key == 'Y') or (generate_key == 'yes') or (generate_key == 'YES')  or (generate_key == 'Yes'):
+        xapi.PanXapi.keygen(self)
+        return
 def file(filename):
     create_file= open(filename,'w')
     create_file.write('hello')
@@ -17,5 +23,6 @@ def file(filename):
 
 # Call main
 if __name__ == '__main__':
-    connection(hostname,username,password)
-    file(filename)
+    api_call(generate_key,self)
+    #connection(hostname,username,password)
+    #file(filename)
