78bc382a7d3f846b4f006c6e5c496092067bf6f3
rskinner <reggie.skinnerjr@gmail.com>
2019-03-23 11:56:57 -0400

    Only Stuff That Works

--- ThisOneWorks.py
+++ ThisOneWorks.py
@@ -1,19 +1,19 @@
 from paramiko import SSHClient
 import getpass
-from pan import xapi
+
+hostname = raw_input('Hostname:')
+username = raw_input('Username:')
+password = getpass.getpass("Password: ")
+filename = raw_input(('File Path/Name:'))
 def connect(hostname,username,password):
         remote = SSHClient()
         remote.load_system_host_keys()
         remote.connect(hostname=hostname, port=22, username=username, password=password)
-def api(self,y, x):
-    self.xapi=show
-    x = xapi.PanXapi.show(self,xpath='show system info')
-    print(x)
+def file(filename):
+    create_file= open(filename,'w')
+    create_file.write('hello')
 def main():
     connect()
-    api()
-hostname = raw_input('Hostname:')
-username = raw_input('Username:')
-password = getpass.getpass("Password: ")
+    file(filename)
 if __name__ == '___main___':
     main()
