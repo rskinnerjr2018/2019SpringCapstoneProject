8ee0646bad0cd366e7f4b3ebcf12bc446708c0a4
rskinner <reggie.skinnerjr@gmail.com>
2019-03-26 13:53:08 -0400

    Rewritten ThisOneWorks

--- OOP_attempt.py
+++ OOP_attempt.py
@@ -1,20 +1,20 @@
 from  paramiko import SSHClient
 import getpass
 
-class CapstoneProject(object):
+class CapstoneProject():
     def __init__(self):
         self.hostname = raw_input('IP Address:')
         self.username = raw_input('Username:')
         self.password = getpass.getpass('Password:')
-        self.filename = raw_input('File Path/Name:')
+        self.filename = open(raw_input('File Path/Name:'), 'w')
         self.port = 22
-    def Connection(hostname,username,password):
+    def Connection(self, hostname, username, password):
         remote = SSHClient()
         remote.load_system_host_keys()
         remote.connect(hostname=hostname, username=username, password=password)
-    def file(filename):
-        create_file = open(filename, 'w')
-        create_file.write('Hello World')
+    def file(self):
+        self.filename.write('Hello World')
+        self.filename.close()
 def main():
     CapstoneProject()
 
